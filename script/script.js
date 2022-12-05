9402840138055b07cc696baa95bdb281c8d821bb
prabakaran <mspk028@gmail.com>
2022-12-05 11:57:34 +0530

    add ok button

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ const btnSubmit = document.getElementById("btn-submit");
 const hiddenEl = document.getElementById("hidden-card");
 const cardEl = document.getElementById("show");
 const outputEl = document.getElementById("output");
+const btnOk = document.getElementById("btnok");
 
 //global variable
 let rating = 0;
@@ -14,6 +15,7 @@ let rating = 0;
 for (let i = 0; i < inputBtnEl.length; i++) {
   inputBtnEl[i].addEventListener("click", function () {
     rating = Number(this.value);
+    this.style.transform = "scale(0.95) translateZ(-5px)";
     this.style.backgroundColor = "#fb7413";
     this.style.color = "#ffffff";
   });
@@ -27,4 +29,14 @@ btnSubmit.addEventListener("click", function () {
   hiddenEl.classList.add("show");
 
   outputEl.innerText = `You Selected ${rating} out of 5`;
+});
+
+btnOk.addEventListener("click", function () {
+  cardEl.classList.remove("hidden");
+  cardEl.classList.add("show");
+
+  hiddenEl.classList.remove("show");
+  hiddenEl.classList.add("hidden");
+
+  inputBtnEl.value = null;
 });
